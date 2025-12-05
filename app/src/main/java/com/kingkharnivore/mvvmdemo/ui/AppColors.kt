230a2d948f1d7e7c3c644d9b7fe21ca0387249c4
Rajas Kharkar <rajaskharkar278@gmail.com>
2025-12-05 01:18:32 -0600

    Making UI sexy

---
+++ app/src/main/java/com/kingkharnivore/mvvmdemo/ui/AppColors.kt
@@ -0,0 +1,14 @@
+package com.kingkharnivore.mvvmdemo.ui
+
+import androidx.compose.ui.graphics.Color
+
+object AppColors {
+    val DarkBackground = Color(0xFF0F172A)
+    val PurplePrimary = Color(0xFF7C3AED)
+    val PurpleSecondary = Color(0xFF9333EA)
+    val PinkAccent = Color(0xFFEC4899)
+    val CardBackground = Color(0xFF1E293B)
+    val TextPrimary = Color(0xFFFFFFFF)
+    val TextSecondary = Color(0xFFC4B5FD)
+    val BorderColor = Color(0x337C3AED)
+}
